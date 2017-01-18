36b09005ef5d3b5fe1d9abd9e5a36abce97f67ad
Julio Trecenti <julio.trecenti@gmail.com>
2017-01-18 18:07:12 -0200

    copy default files and renders markdown things

--- R/setup.R
+++ R/setup.R
@@ -1,36 +1,20 @@
 #' setup power up
 #'
-#' Cria o esqueleto para um power-up em uma pasta
+#' Cria o esqueleto para um power-up em uma pasta.
 #'
-#' @param path pasta para criar o power-up. A última parte é o nome do power-up. Recomenda-se fazer na forma puXXX, em camelCode.
-#' @param rstudio criar arquivo .Rproj?
+#' @param name nome do PU, que será transformado em pu."name".
+#' @param dir pasta para criar o power-up. A última parte é o nome do PU. Recomenda-se fazer na forma pu.XXX, em camelCode.
 #'
 #' @import devtools
-#'
 #' @export
-pu_setup <- function(path = '.', rstudio = TRUE) {
-  parent_dir <- normalizePath(dirname(path), winslash = "/",
-                              mustWork = TRUE)
-  dir.create(file.path(path, 'figure'), showWarnings = FALSE)
-  dir.create(file.path(path, 'data'), showWarnings = FALSE)
-  dir.create(file.path(path, 'R'), showWarnings = FALSE)
-  if (rstudio) {
-    proj <- system.file('template/proj.Rproj', package = 'puTemplate', mustWork = TRUE)
-    file.copy(proj, sprintf('%s/%s.Rproj', path, basename(path)))
-    ugi(c(".Rproj.user", ".Rhistory", ".RData"), directory = path)
-  }
+pu_setup <- function(name, dir = '.') {
+  parent_dir <- normalizePath(dirname(dir), winslash = "/", mustWork = TRUE)
+  zip_file <- system.file('template/proj.zip', package = 'puTemplate', mustWork = TRUE)
+  utils::unzip(zip_file, exdir = dir)
   readme <- system.file('template/README.Rmd', package = 'puTemplate', mustWork = TRUE)
-  rmarkdown::render(readme,
-                    output_format = rmarkdown::md_document(),
-                    output_dir = path, quiet = TRUE)
+  rmarkdown::render(readme, output_format = rmarkdown::md_document(),
+                    output_dir = dir, quiet = TRUE)
+  arq_rstudio <- dir(dir, pattern = 'proj\\.Rproj', full.names = TRUE)
+  file.rename(arq_rstudio, sprintf('%s/%s.Rproj', dir, name))
   invisible(TRUE)
 }
-
-#' copia de use_git_ignore
-#'
-#' @param ignores lista dos ignores a serem adicionados.
-#' @param directory diretorios.
-#' @param pkg objeto do tipo pacote.
-#'
-#' @export
-ugi <- devtools:::use_git_ignore
